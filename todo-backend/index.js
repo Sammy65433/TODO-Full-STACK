59810ae9b9e5c2564aa0e698b3ec9c46b72dc8da
Sammy65433 <samiamlistening@gmail.com>
2026-07-31 11:13:12 -0400

    connect frontend to backend 122

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -3,8 +3,9 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './db.js'
 import todoRoutes from './routes/todo.js'
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
