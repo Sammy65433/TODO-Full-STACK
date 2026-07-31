d963089d5562b452439ca15d6dab9e53fbb88751
Sammy65433 <samiamlistening@gmail.com>
2026-07-31 09:26:33 -0400

    connect frontend to backend 119

--- todo-backend/models/todo.js
+++ todo-backend/models/todo.js
@@ -1,16 +1,21 @@
 import mongoose from 'mongoose'
 
-const todoSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: true
+const todoSchema = new mongoose.Schema(
+    {
+        text: {
+            type: String,
+            required: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
     },
-    completed: {
-        type: Boolean,
-        default: false
+    {
+        timestamps: true
     }
-})
+)
 
 const Todo = mongoose.model('Todo', todoSchema)
 
-export default Todo;
+export default Todo
